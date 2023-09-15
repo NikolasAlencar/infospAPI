331ce76a98b04d3863f942d71bdd0686c9774545
NikolasAlencar <nikolasalencarrs73@gmail.com>
2023-09-15 12:50:38 -0300

    feat: prepara web socket

--- server/Server.ts
+++ server/Server.ts
@@ -1,7 +1,9 @@
 import { router } from './routes';
 import { enviroment } from './enviroments/enviroment';
-
+import { getCollection } from './shared/middleware/Firestore';
+import { Server } from 'socket.io';
 import express from 'express';
+import http from 'http';
 
 const cors = require('cors')
 const admin = require("firebase-admin");
@@ -21,3 +23,19 @@ server.use(express.json());
 server.use(router);
 
 export { server };
+
+const httpServer = new http.Server(server);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', (client: any) => {
+  io.emit('Teste', {message: 'Teste'});
+});
+
+const doc = getCollection('notificacoes').doc('1YL5L3Bhmna4ybUAfFv2');
+
+const observer = doc.onSnapshot((docSnapshot: any) => {
+  console.log(`Received doc snapshot: ${docSnapshot.get('testes')}`);
+  io.emit('Teste', docSnapshot);
+}, (err: any) => {
+  console.log(`Encountered error: ${err}`);
+});
